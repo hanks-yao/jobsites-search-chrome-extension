1eedb1ad49009532b06dfc267cc3c29017a8bcf3
Yao Yuan <380118281@qq.com>
2019-12-17 16:49:14 +0800

    fix linkedin && add xlsx.js

--- content_script/content_script_linkedin.js
+++ content_script/content_script_linkedin.js
@@ -1,216 +1,224 @@
 console.log('linkedin content_scritp loaded!');
 
 const fun = {
-	getJobsMaster: async function(pages, filter){
-		const target = this.getCurrentPage() + pages - 1; // 抓到的页数， 10就是抓到第10页，linkedin最多40页
-		let result = [];
-		// let current = this.getCurrentPage();
+  result: [],
+  getJobsMaster: async function(pages, filter){
+    const target = this.getCurrentPage() + pages - 1; // 抓到的页数， 10就是抓到第10页，linkedin最多40页
+    let result = [];
+    // let current = this.getCurrentPage();
 
-		while(this.getCurrentPage() <= target) {
-			const isBottom = await this.scrollToBottom();
-			if (!isBottom) {
-				continue;
-			};
+    while(this.getCurrentPage() <= target) {
+      const isBottom = await this.scrollToBottom();
+      if (!isBottom) {
+        break;
+      };
 
-			result = result.concat(this.getJobsInfo());
+      this.result = this.result.concat(this.getJobsInfo());
 
-			if(this.goToNextPage()) {
-				do{
-					await this.delayXSeconds(1);
-				} while (this.isPageLoading())
-			} else {
-				// 没有下一页跳出循环
-				break;
-			}
-		}
+      if(this.goToNextPage()) {
+        do{
+          await this.delayXSeconds(1);
+        } while (this.isPageLoading())
+      } else {
+        // 没有下一页跳出循环
+        break;
+      }
+    }
 
-		console.log(result);
-		return result;
-	},
-	getJobsInfo: function() {
-		const $items = $('ul.jobs-search-results__list > li');
-		let result = [];
+    console.log(this.result);
+    return this.result;
+  },
+  getJobsInfo: function() {
+    const $items = $('ul.jobs-search-results__list > li');
+    let result = [];
 
-		if (!$items.length) {return []}
+    if (!$items.length) {return []}
 
-		for (let i = 0; i < $items.length; i++) {
-			let $item = $($items[i]),
- 				element = {};
+    for (let i = 0; i < $items.length; i++) {
+      let $item = $($items[i]),
+        element = {};
 
- 			const $wrapper = $item.find('.job-card-search__content-wrapper');
+      const $wrapper = $item.find('.job-card-search__content-wrapper');
 
- 			element['job_title'] = $wrapper.find('.job-card-search__title > a').contents().filter(function (index, content) {return content.nodeType === 3;}).text().trim(); // 过滤子元素文本文本;
- 			element['company'] = $wrapper.find('.job-card-search__company-name').text().trim();
- 			element['company_link'] = $wrapper.find('.job-card-search__company-name > a').attr('href');
- 			element['about_link'] = fun.changeLinkToAbout(element['company_link']);
- 			element['location'] = $wrapper.find('.job-card-search__location').text().trim();
+      element['job_title'] = $wrapper.find('.job-card-search__title > a').contents().filter(function (index, content) {return content.nodeType === 3;}).text().trim(); // 过滤子元素文本文本;
+      element['company'] = $wrapper.find('.job-card-search__company-name').text().trim();
+      element['company_link'] = $wrapper.find('.job-card-search__company-name > a').attr('href');
+      element['about_link'] = fun.changeLinkToAbout(element['company_link']);
+      element['location'] = $wrapper.find('.job-card-search__location').text().trim();
 
- 			result.push(element);
- 		}
+      result.push(element);
+    }
 
- 		console.log(result);
-		return result;
-	},
-	scrollToBottom: async function(){
-		const $scrollDom = $('.jobs-search-results');
-		const scrollDom = $scrollDom[0];
-		const scrollHeight = scrollDom.scrollHeight; // 异步加载更多数据会变化
-		const clientHeight = scrollDom.clientHeight;
-		// scrollHeight = clientHeight + srollTop时，滚动到最底部;
+    console.log(result);
+    return result;
+  },
+  scrollToBottom: async function(){
+    const $scrollDom = $('.jobs-search-results');
+    const scrollDom = $scrollDom[0];
+    if (!scrollDom) return false;
 
-		scrollDom.scrollTop = 0;
-		await this.delayXSeconds(1);
+    const scrollHeight = scrollDom.scrollHeight; // 异步加载更多数据会变化
+    const clientHeight = scrollDom.clientHeight;
+    // scrollHeight = clientHeight + srollTop时，滚动到最底部;
 
-		while(Math.ceil(scrollDom.scrollTop + scrollDom.clientHeight) < scrollDom.scrollHeight) {
-			console.log('scrolling');
+    scrollDom.scrollTop = 0;
+    await this.delayXSeconds(1);
 
-			// $scrollDom.animate({scrollTop: (scrollDom.scrollHeight - clientHeight) }, 1000);
-			scrollDom.scrollTop = scrollDom.scrollTop + 500;
-			await this.delayXSeconds(1);
-		}
+    while(Math.ceil(scrollDom.scrollTop + scrollDom.clientHeight) < scrollDom.scrollHeight) {
+      console.log('scrolling');
 
-		return true;
-	},
-	goToNextPage: function() {
-		const $current = $('.artdeco-pagination__pages > .artdeco-pagination__indicator.active');
-		const $next = $current.next();
+      // $scrollDom.animate({scrollTop: (scrollDom.scrollHeight - clientHeight) }, 1000);
+      scrollDom.scrollTop = scrollDom.scrollTop + 500;
+      await this.delayXSeconds(1);
+    }
 
-		if ($next.length) {
-			$next.children('button').click();
-			return true;
-		}
+    return true;
+  },
+  goToNextPage: function() {
+    const $current = $('.artdeco-pagination__pages > .artdeco-pagination__indicator.active');
+    const $next = $current.next();
 
-		return false;
-	},
-	getTotalPage: function() {
-		const total  = $('.artdeco-pagination__pages > .artdeco-pagination__indicator:last > span:first').text().trim();
+    if ($next.length) {
+      $next.children('button').click();
+      return true;
+    }
 
-		console.log('total: ', total);
-		return parseInt(total, 10);
-	},
-	getCurrentPage: function() {
-		const current = $('.artdeco-pagination__pages > .artdeco-pagination__indicator.active > span:first').text().trim();
+    return false;
+  },
+  getTotalPage: function() {
+    const total  = $('.artdeco-pagination__pages > .artdeco-pagination__indicator:last > span:first').text().trim();
 
-		return parseInt(current, 10);
-	},
-	isPageLoading: function() {
-		return $('.job-search-ext').hasClass('job-search-ext--loading');
-	},
-	isLastPage: function() {
-		return this.getTotalPage() === this.getCurrentPage();
-	},
-	delayXSeconds(x) {
-	  return new Promise(resolve => {
-	    setTimeout(() => {
-	      resolve();
-	    }, x*1000);
-	  });
-	},
-	includeKeywords: function(target, keywords) {
-		let t = target.toLocaleLowerCase();
+    console.log('total: ', total);
+    return parseInt(total, 10) || 1;
+  },
+  getCurrentPage: function() {
+    const current = $('.artdeco-pagination__pages > .artdeco-pagination__indicator.active > span:first').text().trim();
 
-	 	for ( let n in keywords) {
-			let k = keywords[n].toLocaleLowerCase();
+    return parseInt(current, 10) || 1;
+  },
+  isPageLoading: function() {
+    return $('.job-search-ext').hasClass('job-search-ext--loading');
+  },
+  isLastPage: function() {
+    return this.getTotalPage() === this.getCurrentPage();
+  },
+  delayXSeconds(x) {
+    return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, x*1000);
+    });
+  },
+  includeKeywords: function(target, keywords) {
+    let t = target.toLocaleLowerCase();
 
-			if (t.indexOf(k) > -1) {return true}
-		}
+    for ( let n in keywords) {
+      let k = keywords[n].toLocaleLowerCase();
 
-		return false;
-	},
-	changeLinkToAbout:function(link) {
-		if (!link || link === '#') {return;}
+      if (t.indexOf(k) > -1) {return true}
+    }
 
-		let a = link,
-			index = a.indexOf('?');
+    return false;
+  },
+  changeLinkToAbout:function(link) {
+    if (!link || link === '#') {return;}
 
-		if (index !== -1) {
-			a = a.substring(0, index);
-		}
+    let a = link,
+      index = a.indexOf('?');
 
-		if (a.indexOf('http') === -1) {
-			a = 'https://www.linkedin.com' + a + 'about';
-		} else {
-			a = a + 'about';
-		}
+    if (index !== -1) {
+      a = a.substring(0, index);
+    }
 
-		return a;
-	},
+    if (a.indexOf('http') === -1) {
+      a = 'https://www.linkedin.com' + a + 'about';
+    } else {
+      a = a + 'about';
+    }
 
-	getCompanyInfo: function(){
-	  let res = {};
-	  let $detialBlock = $('.org-grid__core-rail--no-margin-left > section > dl');
-	  // let $detialBlock2 = $('#company-details-section');
+    return a;
+  },
 
-	  if ($detialBlock.length) {
-	    let items = $detialBlock.children('dt');
+  getCompanyInfo: function(){
+    let res = {};
+    let $detialBlock = $('.org-grid__core-rail--no-margin-left > section > dl');
+    // let $detialBlock2 = $('#company-details-section');
 
-	    for (let i = 0; i < items.length; i++) {
-	      let $item = $(items[i]),
-	          key = $item.text().trim().toLowerCase().replace(' ', '_'),
-	          value = $item.next('dd').text().trim();
+    if ($detialBlock.length) {
+    let items = $detialBlock.children('dt');
 
-	      if (key == 'website') {
-	        // const website = $item.next('dd').children('a').attr('href');
-	        // res['website'] = website;
+    for (let i = 0; i < items.length; i++) {
+      let $item = $(items[i]),
+        key = $item.text().trim().toLowerCase().replace(' ', '_'),
+        value = $item.next('dd').text().trim();
 
-	        res['domain'] = ufn.getUrlDomamin(value);
-	      } else {
-	        res[key] = value;
-	      }
-	    }
-	  }
+      if (key == 'website') {
+      // const website = $item.next('dd').children('a').attr('href');
+      // res['website'] = website;
 
-	  console.log(res);
-	  return res;
-	},
+      res['domain'] = ufn.getUrlDomamin(value);
+      } else {
+      res[key] = value;
+      }
+    }
+    }
+
+    console.log(res);
+    return res;
+  },
 };
 
 
 
 $(document).ready(function() {
-	console.log('document ready!');
+  console.log('document ready!');
 });
 
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
 
-    if (request.crawlJobsInfo) { //新开的window监听backgournd发送抓取detail的请求
-      console.log("crawlJobsInfo:");
-      console.log(request);
+  if (request.crawlJobsInfo) { //新开的window监听backgournd发送抓取detail的请求
+    console.log("crawlJobsInfo:");
+    console.log(request);
 
-      let {pages, filter} = request;
+    let {pages, filter} = request;
 
-      fun.getJobsMaster(pages, filter).then((res) => {
-	      console.log(res);
-	      sendResponse(res);
-      });
-    } else if(request.crawlCompanyInfo) { //新开的window监听backgournd发送抓取basis的请求
-      console.log("crawlCompanyInfo:");
-      console.log(request);
-
-      let res = fun.getCompanyInfo();
-
+    fun.getJobsMaster(pages, filter).then((res) => {
       console.log(res);
       sendResponse(res);
-    } else if (request.downloadFile) {
-      console.log('downloadFile');
-      console.log(request);
+    });
+  } else if(request.crawlCompanyInfo) { //新开的window监听backgournd发送抓取basis的请求
+    console.log("crawlCompanyInfo:");
+    console.log(request);
 
-      let data = request.data;
+    let res = fun.getCompanyInfo();
 
-      ufn.exportCsv({
-        // title:['Title','Company','Location','Industry','Headquarters','Employees','Type','Founded','Specialties','Domain','AboutLink'],
-        // titleForKey:['job_title','company','location','industry','headquarters','company_size','type','founded','specialties','domain','about_link'],
-        title:['Title','Company','Location','AboutLink'],
-        titleForKey:['job_title','company','location','about_link'],
-        data: data,
-        fileName: 'Linkedin-jobs',
-      });
+    console.log(res);
+    sendResponse(res);
+  } else if (request.downloadFile) {
+    console.log('downloadFile');
+    console.log(request);
 
-      sendResponse(true);
+    let data = request.data;
+    for (var i = 0; i < data.length; i++) {
+      delete data[i].id;
     }
 
-    return true;
+    ufn.downloadXlsx(data, 'jobsites-linkedin.xlsx');
+
+    // ufn.exportCsv({
+    //   // title:['Title','Company','Location','Industry','Headquarters','Employees','Type','Founded','Specialties','Domain','AboutLink'],
+    //   // titleForKey:['job_title','company','location','industry','headquarters','company_size','type','founded','specialties','domain','about_link'],
+    //   title:['Title','Company','Location','AboutLink'],
+    //   titleForKey:['job_title','company','location','about_link'],
+    //   data: data,
+    //   fileName: 'Linkedin-jobs',
+    // });
+
+    sendResponse(true);
+  }
+
+  return true;
 
 });
